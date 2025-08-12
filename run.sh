3a4507ab54a36a0ea4161da0e3754c4af6b35ef5
Wei Jian Hoe <wjhoe@c2r7s1.42singapore.sg>
2025-08-12 14:21:11 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+clear
+if [ "$1" = "n" ] || [ "$1" = "-n" ]; then
+	norminette
+else
+	echo
+	if make re; then
+		clear
+		if [ $# -eq 0 ]; then
+			./philo/philo 5 800 200 200
+		elif [ "$1" = "-v" ]; then
+			valgrind ./philo/philo "${@:2}"
+		elif [ "$5" = "-v" ]; then
+			valgrind ./philo/philo "$@[1, 4]"
+		elif [ "$6" = "-v" ]; then
+			valgrind ./philo/philo "$@[1, 5]"
+		else 
+			./philo/philo "$@"
+		fi
+	fi
+fi
+
+echo "completed"
